c360ac8a52ce6504db6a546ed96069a028fd91a6
attoyibi <43211768+attoyibi@users.noreply.github.com>
2025-03-15 18:53:55 +0700

    vuetify intallation

---
+++ landingpage/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <HomeView />
+      <DataBinding />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import HomeView from './views/HomeView.vue'
+import AppBar from '@/components/AppBar.vue'
+import DataBinding from '@/components/DataBinding.vue'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ landingpage/src/components/AppBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app-bar density="comfortable">
+    <v-app-bar-title> v app bar</v-app-bar-title>
+  </v-app-bar>
+</template>
