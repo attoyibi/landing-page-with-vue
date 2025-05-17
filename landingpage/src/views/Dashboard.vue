f02ff6ba97fea67c29b88850d4936f4bb39dcde3
alterra-muchson <muhammadmuchson@gmail.com>
2025-05-17 13:54:37 +0700

    remove folder

--- landingpage/src/views/Dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-      <h2>Dashboard</h2>
-      <p>Selamat datang di halaman dashboard!</p>
-    </div>
-  </template>
--- landingpage/src/views/ProfileView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-      <h2>Profile</h2>
-      <p>Ini halaman profil pengguna.</p>
-    </div>
-  </template>
-  
